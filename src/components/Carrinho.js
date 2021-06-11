c9d79f62d41154506de85f2c70bde38eea6bbca9
Izabella <bellacbrandao@gmail.com>
2021-06-11 15:20:43 -0300

    adicionando Logica de soma

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -4,9 +4,18 @@ export class Carrinho extends React.Component{
 
     render(){
 
+        let soma=0
+
+        for(let i= 0; i < this.props.dadosCarrinho.length; i++){
+            const itemNoCarrinho = this.props.dadosCarrinho[i]
+            const preco = itemNoCarrinho.value
+            const quantidade = itemNoCarrinho.quantidade
+            soma = soma + preco*quantidade
+        }
+
         const imprimirCompras = this.props.dadosCarrinho.map((produto) => {
             return<div key={produto.id}>
-                <spam> {produto.quantidade}x {produto.name}</spam>
+                <spam> {produto.quantidade} x {produto.name}</spam>
                 <button>Remover</button>
             </div>
         })
@@ -17,7 +26,7 @@ export class Carrinho extends React.Component{
 
             {imprimirCompras}
 
-            <p>Valor Total: R$ ....</p>
+            <p>Valor Total: R$ {soma}</p>
         </div>
     }
 }
